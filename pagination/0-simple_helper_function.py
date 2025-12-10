cb374bdcdd6e36effbac52efa34152b49c7bbc7b
GraceKayem <11557@holbertonstudents.com>
2025-12-10 22:39:04 +1100

    save

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Write a function named index_range that takes two integer arguments
+page and page_size.
+"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Return a tuple containing a start index and an end index corresponding
+    to the range of indexes to return in a list for the given page and
+    page_size.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""Replicate code from the previous task."""
+
+import csv
+import math
+from typing import List
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Implement a get_hyper method that takes the same arguments (and defaults)
+    as get_page and returns a dictionary containing the following key-value pairs.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE, encoding="utf-8") as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the appropriate page of the dataset."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        return self.dataset()[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Return a dictionary containing hypermedia pagination info."""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
