f1a3a9848c09ef02b3cd9ce12974fb1298b4ba36
GraceKayem <11557@holbertonstudents.com>
2026-01-05 01:27:06 +1100

    save

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+getListStudents() {
+  const values = [
+    [id, firstName, location],
+    ["Guillaume", 1, "San Francisco"],
+    ["James", 2, "Columbia"],
+    ["Serena", 5, "San Francisco"]
+  ];
+
+}
