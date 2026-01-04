11789be239d8af051b0ccbd6e33acfc3dd4fe36e
GraceKayem <11557@holbertonstudents.com>
2026-01-05 03:48:57 +1100

    save

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,6 +2,6 @@ export default function getListStudentIds(students) {
   if (!Array.isArray(students)) return [];
 
   return students
-    .filter(student => student && student.id !== undefined)
+    .filter(student => student && typeof student.id === 'number')
     .map(student => student.id);
 }
--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,11 @@
 export default function getStudentsByLocation(students, city) {
   if (!Array.isArray(students)) return [];
 
-  return students.filter(student => student && student.location === city);
+  return students
+    .filter(student => student && student.location === city)
+    .map(student => ({
+      id: student.id,
+      firstName: student.firstName,
+      location: student.location
+    }));
 }
