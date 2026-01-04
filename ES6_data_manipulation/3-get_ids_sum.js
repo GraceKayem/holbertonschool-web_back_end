55281832c12d819c6243dcec883f9b902580d959
GraceKayem <11557@holbertonstudents.com>
2026-01-05 03:53:34 +1100

    save

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,10 +1,3 @@
 export default function getStudentIdsSum(students) {
-  if (!Array.isArray(students)) return 0;
-
-  return students.reduce((total, student) => {
-    if (student && typeof student.id === 'number') {
-      return total + student.id;
-    }
-    return total; 
-  }, 0);
+  return students.reduce((sum, student) => sum + (student.id || 0), 0);
 }
