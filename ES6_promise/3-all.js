98311a3cba5438773c706cdcb760d71b286735de
GraceKayem <11557@holbertonstudents.com>
2026-01-11 20:15:25 +1100

    save

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,22 +1,16 @@
 import { uploadPhoto, createUser } from './utils.js'; 
 
 export default function handleProfileSignup() {
-  let body;
-  let firstName;
-  let lastName;
-
   return uploadPhoto()
     .then((data) => {
-      body = data.body;
-      return createUser();
-    })
-    .then((user) => {
-      firstName = user.firstName;
-      lastName = user.lastName;
-      console.log(`${body}, ${firstName}, ${lastName}`);
-      return { body, firstName, lastName };
+      const body = data.body;
+      return createUser().then((user) => {
+        const firstName = user.firstName;
+        const lastName = user.lastName;
+        console.log(`${body} ${firstName} ${lastName}`);
+      });
     })
     .catch(() => {
-      throw new Error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
