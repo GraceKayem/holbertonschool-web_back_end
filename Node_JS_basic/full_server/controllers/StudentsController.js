3de2a67f18b23334ba96887ec2328112b0e4d4c3
GraceKayem <11557@holbertonstudents.com>
2026-01-18 23:03:57 +1100

    save

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,4 +1,4 @@
-import { readDatabase } from '../utils.js';
+import readDatabase from '../utils';
 
 export default class StudentsController {
   static async getAllStudents(req, res) {
@@ -15,15 +15,15 @@ export default class StudentsController {
         responseText += `Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`;
       });
 
-      res.status(200).send(responseText.trim());
+      return res.status(200).send(responseText.trim());
     } catch (err) {
-      res.status(500).send('Cannot load the database');
+      return res.status(500).send('Cannot load the database');
     }
   }
 
   static async getAllStudentsByMajor(req, res) {
     const dbFile = process.argv[2];
-    const major = req.params.major;
+    const { major } = req.params;
 
     if (!['CS', 'SWE'].includes(major)) {
       return res.status(500).send('Major parameter must be CS or SWE');
@@ -32,9 +32,9 @@ export default class StudentsController {
     try {
       const data = await readDatabase(dbFile);
       const students = data[major] || [];
-      res.status(200).send(`List: ${students.join(', ')}`);
+      return res.status(200).send(`List: ${students.join(', ')}`);
     } catch (err) {
-      res.status(500).send('Cannot load the database');
+      return res.status(500).send('Cannot load the database');
     }
   }
 }
--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,30 +1,30 @@
 import fs from 'fs';
 
-export function readDatabase(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(err);
+      return;
+    }
 
-      const lines = data.trim().split('\n');
-      const headers = lines[0].split(',');
-      const fieldIndex = headers.indexOf('field');
-      const firstNameIndex = headers.indexOf('firstname');
+    const lines = data.trim().split('\n');
+    const headers = lines[0].split(',');
+    const fieldIndex = headers.indexOf('field');
+    const firstNameIndex = headers.indexOf('firstname');
 
-      const result = {};
+    const result = {};
 
-      for (let i = 1; i < lines.length; i += 1) {
-        const cols = lines[i].split(',');
-        const field = cols[fieldIndex];
-        const firstname = cols[firstNameIndex];
+    for (let i = 1; i < lines.length; i += 1) {
+      const cols = lines[i].split(',');
+      const field = cols[fieldIndex];
+      const firstname = cols[firstNameIndex];
 
-        if (!result[field]) result[field] = [];
-        result[field].push(firstname);
-      }
+      if (!result[field]) result[field] = [];
+      result[field].push(firstname);
+    }
 
-      resolve(result);
-    });
+    resolve(result);
   });
-}
+});
+
+export default readDatabase;
