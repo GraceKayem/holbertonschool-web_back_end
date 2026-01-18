c6899a4edc750e6e7fa5d7f3a645a663b777843e
GraceKayem <11557@holbertonstudents.com>
2026-01-18 23:00:55 +1100

    save

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,40 @@
+import { readDatabase } from '../utils.js';
+
+export default class StudentsController {
+  static async getAllStudents(req, res) {
+    const dbFile = process.argv[2];
+
+    try {
+      const data = await readDatabase(dbFile);
+      const fields = Object.keys(data).sort((a, b) =>
+        a.toLowerCase().localeCompare(b.toLowerCase())
+      );
+
+      let responseText = 'This is the list of our students\n';
+      fields.forEach((field) => {
+        responseText += `Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`;
+      });
+
+      res.status(200).send(responseText.trim());
+    } catch (err) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    const dbFile = process.argv[2];
+    const major = req.params.major;
+
+    if (!['CS', 'SWE'].includes(major)) {
+      return res.status(500).send('Major parameter must be CS or SWE');
+    }
+
+    try {
+      const data = await readDatabase(dbFile);
+      const students = data[major] || [];
+      res.status(200).send(`List: ${students.join(', ')}`);
+    } catch (err) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+}
